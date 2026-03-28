580e3370d140b6d7d2607c70e4a47d06fb690810
Angie Escobar <escobarav103@gmail.com>
2026-03-27 22:04:01 -0500

    fix conexion bd

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+const db = require('./models');
+
+db.sequelize.authenticate()
+  .then(() => console.log("Conectado a la BD"))
+  .catch(err => console.log("Error BD:", err));
+
 const alquilerR = require('./routes/alquilerroutes');
 const autosR = require('./routes/autosroutes');
 const clientesR = require('./routes/clienteroutes');
@@ -18,8 +24,13 @@ app.use('/api/alquiler', alquilerR);
 app.use('/api/autos', autosR);
 app.use('/api/clientes', clientesR);
 
+app.get('/', (req, res) => {
+  res.send('API funcionando');
+});
+
 // Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
