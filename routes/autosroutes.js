fedb98502a3cb0e106871e497c292f444c160662
Angie Escobar <escobarav103@gmail.com>
2026-03-27 22:38:04 -0500

    fix rutas autos

--- routes/autosroutes.js
+++ routes/autosroutes.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const autoController = require('../controller/autoscontroller');
+const autosController = require('../controller/autoscontroller');
 
-// Ruta para obtener autos disponibles
-router.get('/', autoController.autosDisponibles);
-
-// Ruta para registrar un nuevo auto
-router.post('/', autoController.registrarAuto);
+router.get('/', autosController.autosDisponibles);
+router.post('/crear', autosController.registrarAuto);
 
 module.exports = router;
