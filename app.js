10258f8478c1478dc6a7c486db19cde54555ccc9
Angie Escobar <escobarav103@gmail.com>
2026-03-30 21:56:31 -0500

    fix estructura

--- app.js
+++ app.js
@@ -2,27 +2,20 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const sequelize = require("./config/db");
 const autosRoutes = require("./routes/autosRoutes");
 
 const app = express();
-app.use("/api/autos", autosRoutes);
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/autos", autosRoutes);
 
-// Ruta base (IMPORTANTE para evitar error en Render)
+// ruta base
 app.get("/", (req, res) => {
   res.send("API funcionando 🚀");
 });
 
-// Conectar BD
-sequelize.sync()
-  .then(() => console.log("BD conectada"))
-  .catch(err => console.log(err));
-
 app.listen(process.env.PORT, () => {
-  console.log("Servidor corriendo en puerto " + process.env.PORT);
+  console.log("Servidor corriendo");
 });
